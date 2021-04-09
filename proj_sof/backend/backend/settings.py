efdd628e5a8087980ff80cc88eb7e9751b592d72
joswan2567 <55920014+joswan2567@users.noreply.github.com>
2021-04-08 22:11:22 -0300

    update db

--- proj_sof/backend/backend/settings.py
+++ proj_sof/backend/backend/settings.py
@@ -157,6 +157,10 @@ import django_heroku
 django_heroku.settings(locals())
 
 # Update database configuration with $DATABASE_URL.
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
